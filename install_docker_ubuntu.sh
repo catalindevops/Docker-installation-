34af05f2c9ffb349d1e9ba2c7b2912f5c9cb7366
catalindevops <37984424+catalindevops@users.noreply.github.com>
2018-04-01 21:49:49 +0300

    clean the environment

--- install_docker_ubuntu.sh
+++ install_docker_ubuntu.sh
@@ -1,1 +1,11 @@
+#!/bin/bash
+set -e
+
+# This script is used to install docker CE on ubuntu 16.04
+# First we will remove old instalations if they exists
+
+echo "Installing docker ce on ubuntu"
+echo "=============================="
+echo "Clean the environment "
+apt-get remove docker docker-engine docker.io
 
